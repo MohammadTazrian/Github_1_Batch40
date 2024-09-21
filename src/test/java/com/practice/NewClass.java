ef0f92d3b48d83fbf751ec2dc9ec6fa8ddb8b10a
MohammadTazrian <tazrianahmed1993@gmail.com>
2024-09-20 21:47:33 -0400

    Added NewClass under com.generic

---
+++ src/test/java/com/practice/NewClass.java
@@ -0,0 +1,10 @@
+package com.practice;
+
+public class NewClass {
+
+	public static void main(String[] args) {
+		System.out.println("This is a new class after the intial push to github");
+	}
+	
+	
+}
